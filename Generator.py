9c3a2413692b567b062da78119632b09445f33fd
frankmollard <116736747+frankmollard@users.noreply.github.com>
2024-04-24 10:23:28 +0200

    Update Generator.py
    two distributions

--- Generator.py
+++ Generator.py
@@ -20,49 +20,53 @@ st.write('This is the Generator of a conditional Deep Convolutional GAN')
 
 creatures = {"cat":0, "dog":1, "man":2, "wild":3, "woman":4}
 
-distribution = st.radio(
+distribution = st.sidebar.radio(
     "Kind of Distribution",
     ["Binomial", "Normal"],
     captions = ["clearer results", "more variance"]
 )
 
-m = st.sidebar.select_slider(
-    'Select mean of latent space',
-    options=[-0.1, 0, 0.1],
-    value = 0
-)
-
-s = st.sidebar.select_slider(
-    'Select standard deviation of latent space',
-    options=np.linspace(2,0.1,20), 
-    value = 1
-)
-
-option1 = st.sidebar.selectbox(
-    'Select Creature',
-    #('cat', 'dog', 'man', 'wild', 'woman')
-    creatures.keys() 
-)
-
-option2 = st.sidebar.selectbox(
-    'Select 2nd Creature',
-    ('None', 'cat', 'dog', 'man', 'wild', 'woman')
-)
+if distribution == "Normal":
+    m = st.sidebar.select_slider(
+        'Select mean of latent space',
+        options=[-0.1, 0, 0.1],
+        value = 0
+    )
+    
+    s = st.sidebar.select_slider(
+        'Select standard deviation of latent space',
+        options=np.linspace(2,0.1,20), 
+        value = 1
+    )
+    
+    option1 = st.sidebar.selectbox(
+        'Select Creature',
+        #('cat', 'dog', 'man', 'wild', 'woman')
+        creatures.keys() 
+    )
+    
+    option2 = st.sidebar.selectbox(
+        'Select 2nd Creature',
+        ('None', 'cat', 'dog', 'man', 'wild', 'woman')
+    )
 
 #st.write('You selected:', option1)
 #st.write('You selected:', option2)
 
-if option2 == 'None':
-    option2 = option1
+    if option2 == 'None':
+        option2 = option1
 
 #@st.cache()
-def vizGenImg(g, sd, me, c1: int, c2: int, ttl: str):
+def vizGenImg(g, sd, me, dist, c1: int, c2: int, ttl: str):
     fig, ax = plt.subplots(1,5, figsize=(15*2,3*2))
     for j in range(5):
-        #xx = np.random.randint(2, size=(1, 100))
-        xx = normal(shape=(1, 100), mean=me, stddev=sd)
+        if dist == "Binomial":
+            xx = np.random.randint(2, size=(1, 100))
+            xx = xx.reshape(-1,100)
+        else:
+            xx = normal(shape=(1, 100), mean=me, stddev=sd)
         xc = tf.stack([[creatures[c1]],[creatures[c2]]], axis=1)
-        img = g.predict([xx, xc], verbose=0)#.reshape(-1,100)
+        img = g.predict([xx, xc], verbose=0)
         img = Image.fromarray(((img+1)/2*255).astype('uint8').reshape((64,64,3)))
         ax[j].axis('off')
         ax[j].imshow(img)
@@ -72,5 +76,5 @@ def vizGenImg(g, sd, me, c1: int, c2: int, ttl: str):
 
 if st.sidebar.button("Run", type="primary"):
     for k in range(3):
-        image, _, _=vizGenImg(generator, s, m, option1, option2, ttl=option1 if option1 ==option2 else option1 + " " + option2)
+        image, _, _=vizGenImg(generator, s, m, distribution, option1, option2, ttl=option1 if option1 ==option2 else option1 + " " + option2)
         st.pyplot(fig=image)
